c3d6524e7f0e190859c040970312abdeda0af91e
Robert660 <rmcmullen660@hotmail.ca>
2017-03-10 11:02:08 -0500

    Testing 3/10/17

--- ServerClient/UDPClient.py
+++ ServerClient/UDPClient.py
@@ -8,17 +8,20 @@ def setup(port1,port2,address1,address2):
     s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address2 = (address2, port2)
     s2.bind(server_address2)
+
+    return s1,s2,server_address1
     
 
 def send(sendData):
     data = sendData.strip()
     s1.sendto(data.encode('utf-8'), server_address1)
+    return True
 
 def receive(receiveData):
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port2)
     buf, address = s2.recvfrom(2048)
     print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
-
+    return True
 
 def shutdown():
     s1.shutdown(1)
