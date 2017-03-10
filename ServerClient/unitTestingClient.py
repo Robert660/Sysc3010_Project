6df4a3f633c073ff5dc8a627c5084e76e891f133
Robert660 <rmcmullen660@hotmail.ca>
2017-03-10 10:56:28 -0500

    Testing 3/10/17

--- ServerClient/unitTestingClient.py
+++ ServerClient/unitTestingClient.py
@@ -1,41 +1,28 @@
 import unittest
 import UDPClient
 
-class testSetup(unittest.TestCase):
-    
+def setUpClass(cls):
+    print("")
+
+class testClient(unittest.TestCase):
     def testFail(self):
         with self.assertRaises(TypeError):
             UDPClient.setup(None, None, None, None)
 
+    def sendNull(self):        
+        self.assertFalse(UDPClient.send(None),False)
+
 
     def testPass(self):
-        self.assertTrue(UDPClient.setup(1101,1021, 'localhost', 'localhost') != None, None, None, None)
-
-        
-class testSend(unittest.TestCase):
-
-    def sendNull():
-        
-        self.assertFalse(UDPClient.send(None))
-        UPDClient.shutdown()       
+        self.assertTrue(UDPClient.setup(8080,8080, '127.0.0.1', '127.0.0.1'),True)     
 
 
-   # def sendValid():
-
-
-#class testReceive(unittest.TestCase):
-
-   # def receiveNull():
-
-   # def receiveValid():
-
-        
-
-      
-    #def testPass(self):
+    
+  
 
 if __name__== '__main__':
-   unittest.main()
+    setUpClass(testClient)
+    unittest.main()
         
         
         
